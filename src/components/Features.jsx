075181d687b604f106c955e317e7efa5567fb805
jerry <jry@live.se>
2024-10-24 13:32:11 +0200

    fin justeringar för att göra det korrekt

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,57 +2,86 @@ import React from 'react';
 
 const Features = () => {
   return (
-    <>
-      <section id="page3">
-        <div className="container">
-          <h1 className="headline">How Does It Work?</h1>
+    <section id="page2">
+      <div className="container">
+        <div className="images">
+          <img className="image" src="images/sida2/bigphone.svg" alt="Iphone med visakort" />
+        </div>
 
-          <div className="images">
-            <img className="image1" src="images/sida3/pic1.svg" alt="Iphone with spending graph" />
-            <img className="image2" src="images/sida3/bild2.svg" alt="Iphone with visa card" />
-            <img className="image3" src="images/sida3/pic3.svg" alt="Iphone with latest transactions and balance" />
-          </div>
-
-          <div className="bottomtext">
-            <h4 className="headlinebottom">Latest transaction history</h4>
+        <div className="rightcontent">
+          <div className="content">
+            <h1 className="headline">App Features</h1>
             <p className="text">
-              Enim, et amet praesent pharetra. Mi non ante hendrerit amet sed.<br />
-              Arcu sociis tristique quisque hac in consectetur condimentum.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat mollis egestas. Nam luctus facilisis ultrices. Pellentesque volutpat ligula est. Mattis fermentum, at nec lacus.
             </p>
           </div>
-        </div>
-      </section>
 
-      <section id="page4">
-        <div className="container">
-          <div className="topcontent">
-            <div className="texttop">
-              <h2 className="headlinetop">
-                Make your money<br />transfer simple and clear
-              </h2>
-              <div className="circlecheck">
-                <i className="fa-light fa-circle-check"></i>
-                <span className="text">Banking transactions are free for you</span>
+          <div className="textboxes">
+            <div className="gridbox">
+              <div className="icon-wrapper">
+                <i className="fa-thin fa-credit-card icons"></i>
               </div>
-              <div className="circlecheck">
-                <i className="fa-light fa-circle-check"></i>
-                <span className="text">No monthly cash commission</span>
+              <div className="text-column">
+                <h5>Easy Payments</h5>
+                <p>Id mollis consectetur congue egestas egestas suspendisse blandit justo.</p>
               </div>
-              <div className="circlecheck">
-                <i className="fa-light fa-circle-check"></i>
-                <span className="text">Manage payments and transactions online</span>
-              </div>
+            </div>
 
-              <a id="Learn-more1" href="#" className="learnmore">
-                <span>Learn more</span>
-                <i className="fa-solid fa-arrow-right"></i>
-              </a>
+            <div className="gridbox">
+              <div className="icon-wrapper">
+                <i className="fa-sharp fa-thin fa-shield icons"></i>
+              </div>
+              <div className="text-column">
+                <h5>Data Security</h5>
+                <p>Augue pulvinar justo, fermentum fames aliquam accumsan vestibulum non.</p>
+              </div>
+            </div>
+
+            <div className="gridbox">
+              <div className="icon-wrapper">
+                <i className="fa-thin fa-chart-simple icons"></i>
+              </div>
+              <div className="text-column">
+                <h5>Cost Statistics</h5>
+                <p>Mattis urna ultricies non amet, purus in auctor non. Odio vulputate ac nibh.</p>
+              </div>
+            </div>
+
+            <div className="gridbox">
+              <div className="icon-wrapper">
+                <i className="fa-thin fa-comments-question icons"></i>
+              </div>
+              <div className="text-column">
+                <h5>Support 24/7</h5>
+                <p>A elementum, imperdiet enim, pretium etiam facilisi in aenean quam mauris.</p>
+              </div>
+            </div>
+
+            <div className="gridbox">
+              <div className="icon-wrapper">
+                <i className="fa-thin fa-wallet icons"></i>
+              </div>
+              <div className="text-column">
+                <h5>Regular Cashback</h5>
+                <p>Sit facilisis dolor arcu, fermentum vestibulum arcu elementum imperdiet eleifend.</p>
+              </div>
+            </div>
+
+            <div className="gridbox">
+              <div className="icon-wrapper">
+                <i className="fa-sharp fa-thin fa-face-smile icons"></i>
+              </div>
+              <div className="text-column">
+                <h5>Top Standards</h5>
+                <p>Faucibus cursus maecenas lorem cursus nibh. Sociis sit risus id. Sit facilisis dolor arcu.</p>
+              </div>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
 export default Features;
+``
